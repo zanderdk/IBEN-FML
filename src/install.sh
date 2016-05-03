8514221bb266ac077e6280916c89d012ec6014eb
Alexander Krog <akrog13@student.aau.dk>
2016-05-03 12:52:36 +0200

    run webpack

--- src/install.sh
+++ src/install.sh
@@ -1,4 +1,10 @@
-sudo apt-get install libreadline6 libreadline6-dev gv build-essential libbdd-dev libbdd0c2 libbdd0c2-dbgsym
+sudo apt-get install libreadline6 libreadline6-dev gv build-essential libbdd-dev libbdd0c2
+cd buddy-2.4
+./configure
+make 
+make install
+cd ..
 ./configure
 make
 make install
+
